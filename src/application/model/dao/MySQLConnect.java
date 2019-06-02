d0faed11ada5dd92b2380ec76a2e1058e601bdce
ThierryLegrosORT <50357733+ThierryLegrosORT@users.noreply.github.com>
2019-06-02 15:32:47 +0200

    Update MySQLConnect.java

--- src/application/model/dao/MySQLConnect.java
+++ src/application/model/dao/MySQLConnect.java
@@ -5,13 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySQLConnect {
-	/** * paramètres de connexion:  URL de connection et login, pass pour la BDD */
+	/** * paramÃ¨tres de connexion:  URL de connection et login, pass pour la BDD */
     private static String url = "jdbc:mysql://localhost/videotheque?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    private static String user = "Thierry";
-    private static String passwd = "P97928zg!";
+    private static String user = "root";
+    private static String passwd = "";
     /** * Objet Connection */
     private static Connection connect;
-    /** * Méthode qui va nous retourner notre instance * et la créer si elle     n'existe pas... * @return */
+    /** * MÃ©thode qui va nous retourner notre instance * et la crÃ©er si elle     n'existe pas... * @return */
     public static Connection getInstance(){
         if(connect == null){
         try {
